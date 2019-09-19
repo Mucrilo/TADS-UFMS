e36deb242772cc4236cfe4b90a032d215103a29b
Murilo Santos <mucrilobass@gmail.com>
2019-09-19 19:45:53 -0400

    60 e 61 adicionados

--- 60FX/src/Main.java
+++ 60FX/src/Main.java
@@ -2,27 +2,35 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    ArrayList<String> palavras = new ArrayList<String>();
+	
+	public static void main(String[] args) {
+		ArrayList<String> palavras = new ArrayList<String>();
 
-    palavras.add("AR");
-    palavras.add("BR");
-    palavras.add("JP");
-    palavras.add("US");
-  
-    Scanner scanner = new Scanner(System.in);
-    
-    busca(scanner.nextLine());
-    
-}
-    public boolean busca(String palavra) {
-    	
-    	for (String buscaString : palavras) {
+	    palavras.add("AR");
+	    palavras.add("BR");
+	    palavras.add("JP");
+	    palavras.add("US");
+	  
+	    Scanner scanner = new Scanner(System.in);
+	    
+	    String busca = scanner.nextLine();
+	    
+	    int indexOfBusca = -1;
+	    
+	    for (String buscaString : palavras) {
     		
         	if(buscaString.equals(busca)) {
-        		return true;
+        		System.out.println(true);
+        		indexOfBusca = palavras.indexOf(buscaString);
         	}
-    	}
-    }
-   
-  }
+        }
+	    
+	    if (indexOfBusca == -1) {
+			System.out.println(false);
+		}
+	    
+	    System.out.println(indexOfBusca);
+	    
+	scanner.close(); 
+	}
+}
