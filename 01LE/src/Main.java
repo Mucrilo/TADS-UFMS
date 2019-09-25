2284ef6751d8653576025b4cc52e52295e2e9833
Murilo Santos <mucrilobass@gmail.com>
2019-09-25 17:36:59 -0400

    Revert "Revert "Estudos de um fds.""
    This reverts commit df5c08fc0195cb5f33877f308e08f0fbc49c7090.

--- 01LE/src/Main.java
+++ 01LE/src/Main.java
@@ -1,30 +1,23 @@
 import java.util.Scanner;
 
 class Main {
-	public static void main(String[] args) {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    int num1 = s.nextInt();
+    int num2 = s.nextInt();
     
-		Scanner s = new Scanner(System.in);
-	    int num1 = s.nextInt();
-	    int num2 = s.nextInt();
-	    int acumulador = 0;
-	    
-	    if((num1 % 2) == 0) {
-	    	
-	    	for(int contador = (num1 + 1); contador < num2; contador += 2) {
-	    		
-	    		acumulador += contador;
-	    	}
-	    } else {
-	    	
-	    	for(int contador = num1; contador < num2; contador += 2) {
-	    		
-	    		acumulador += contador;
-	    }
-	    
-	    acumulador += num2;
-	    
-	    System.out.println(acumulador);
-	    
-	    }
+    int soma = 0;
+    
+    if (num1 % 2 == 0) {
+		num1++;
 	}
+    
+    for (int i = num1; i <= num2; i += 2) {
+		
+		soma += i;
+	}
+    
+    System.out.println(soma);
+    
+  }
 }
