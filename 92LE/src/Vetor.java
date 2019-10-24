c2d48d6afa940851cf04954f14009da0741e474c
Murilo Santos <mucrilobass@gmail.com>
2019-10-24 19:26:41 -0400

    Arrumando coisas

---
+++ 92LE/src/Vetor.java
@@ -0,0 +1,154 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Vetor<T> implements List<T> {
+
+	private int quantidade;
+	private T[] vetor;
+
+	@SuppressWarnings("unchecked")
+	public Vetor() {
+		quantidade = 0;
+		vetor = (T[]) new Object[10];
+	}
+
+	public String toString() {
+
+		String s = "Vetor[";
+		for (int i = 0; i < quantidade; i++) {
+			if (i == 0) {
+				s += vetor[i];
+			} else {
+				s += ", " + vetor[i];
+			}
+
+		}
+		return s + "]";
+	}
+
+	@Override
+	public int size() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object[] toArray() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		throw new UnsupportedOperationException();
+
+	}
+
+	@Override
+	public boolean add(T arg0) {
+		if (quantidade >= vetor.length) {
+			throw new ArrayIndexOutOfBoundsException();
+		}
+		vetor[quantidade] = arg0;
+		quantidade++;
+		return true;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends T> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean addAll(int index, Collection<? extends T> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void clear() {
+		throw new UnsupportedOperationException();
+
+	}
+
+	@Override
+	public T get(int index) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public T set(int index, T element) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void add(int index, T element) {
+		throw new UnsupportedOperationException();
+
+	}
+
+	@Override
+	public T remove(int index) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int indexOf(Object o) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int lastIndexOf(Object o) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public ListIterator<T> listIterator() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public ListIterator<T> listIterator(int index) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public List<T> subList(int fromIndex, int toIndex) {
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ 93FX/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.List;
+class Main {
+  public static void main(String[] args) {
+    List<String> palavras = new Vetor<String>();
+    
+    palavras.add("A");
+    palavras.add("B");
+    palavras.add("C");
+    palavras.add("D");
+    palavras.add("E");
+    palavras.add("F");
+    palavras.add("G");
+    palavras.add("H");
+    palavras.add("I");
+    palavras.add("J");
+    palavras.add("K");
+    palavras.add("L");
+    palavras.add("M");
+    
+    
+    System.out.println(palavras);
+  }
+}
